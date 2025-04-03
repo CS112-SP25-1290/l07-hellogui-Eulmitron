788a1fbbe83b46beee8221fb1c3cd04134f520eb
Patrick Eulmi <dyop.vera@gmail.com>
2025-04-03 12:13:54 -0700

    Part 1 Finished and working

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -19,7 +20,13 @@ public class HelloApplication extends Application  { //inheriting core functiona
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
-
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello GUI Patrick");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout,300,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
 
     // TODO: follow steps 22-34 in README.md to create an event handler
